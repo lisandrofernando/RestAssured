0df5fe4ed577f2b6b6fc14fa00da0cd722f14ac3
lisandrofernando <lisandrusfernandus@gmail.com>
2022-02-02 17:21:22 -0600

    Upadting tests

--- Tests/cucumber/Otions/TestRunner.java
+++ Tests/cucumber/Otions/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Tests/features", glue= {"stepDefinitions"}, tags ="@DeletePlace", plugin ="json:target/jsonReports/cucumber-report.json")
+@CucumberOptions(features="Tests/features", glue= {"stepDefinitions"}, tags ="@AddPlace", plugin ="json:target/jsonReports/cucumber-report.json")
 public class TestRunner {
 
 }
